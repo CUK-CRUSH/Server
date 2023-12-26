89bdf8ce2aa514047f18c725a5324a12c6efd052
Feedbaek <nicek789@naver.com>
2023-12-26 11:45:55 +0900

    Init

---
+++ src/main/java/crush/myList/MyListApplication.java
@@ -0,0 +1,13 @@
+package crush.myList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crush/myList/MyListApplicationTests.java
@@ -0,0 +1,13 @@
+package crush.myList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
