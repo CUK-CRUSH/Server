7d297af84d2369509279281db4011e7f4c197e46
Feedbaek <nicek789@naver.com>
2024-01-03 16:37:07 +0900

    Fix: commit 꼬여서 사라진 파일 복구

--- src/main/java/crush/myList/config/EnvBean.java
+++ src/main/java/crush/myList/config/EnvBean.java
@@ -1,2 +1,12 @@
-package crush.myList.config;public class EnvBean {
+package crush.myList.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnvBean {
+    @Value("${spring.cloud.gcp.storage.bucket}")
+    public String BUCKET_NAME;
+    @Value("${spring.cloud.gcp.storage.project-id}")
+    public String PROJECT_ID;
 }
