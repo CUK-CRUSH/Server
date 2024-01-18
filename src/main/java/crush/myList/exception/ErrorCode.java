81ed1e24d416af20a024fb561f3a16b950ad5051
DEMl4N <delfirmamentos@gmail.com>
2024-01-18 15:47:26 +0900

    Feat: HTTP ResponseStatusException 에러 처리 가능한 핸들러 기능 추가

--- src/main/java/crush/myList/exception/ErrorCode.java
+++ src/main/java/crush/myList/exception/ErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     InternalError(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러입니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 리소스를 찾을 수 없습니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "권한이 없습니다.");
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "권한이 없습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근할 수 없습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 리소스를 찾을 수 없습니다."),;
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/crush/myList/exception/GlobalExceptionHandler.java
+++ src/main/java/crush/myList/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import crush.myList.global.dto.ResponseBody;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j(topic = "GlobalExceptionHandler")
 @RestControllerAdvice
@@ -13,5 +14,11 @@ public class GlobalExceptionHandler {
     public ResponseBody handleNullPointerException(NullPointerException e){
         log.error(e.getMessage());
         return ErrorResponse.toResponse(ErrorCode.InternalError);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseBody handleResponseStatusException(ResponseStatusException e){
+        log.error(e.getMessage());
+        return ErrorResponse.toResponse(e);
     }
 }
