69784850442b2b8d6ed9f215e50f3136ba8a153d
Feedbaek <nicek789@naver.com>
2024-01-26 15:59:43 +0900

    Feat: 플레이 리스트 음악 조회 시 페이징 적용

--- src/main/java/crush/myList/domain/music/Controller/MusicController.java
+++ src/main/java/crush/myList/domain/music/Controller/MusicController.java
@@ -32,7 +32,8 @@ public class MusicController {
             @ApiResponse(responseCode = "200", description = "음악 조회 성공"),
             @ApiResponse(responseCode = "404", description = "음악 조회 실패", content = @Content(schema = @Schema(hidden = true)))
     })
-    public JsonBody<List<MusicDto.Result>> getMusics(@PathVariable Long playlistId, @RequestParam(required = false, defaultValue = "0") int page) {
+    public JsonBody<List<MusicDto.Result>> getMusics(@PathVariable Long playlistId,
+                                                     @RequestParam(required = false, defaultValue = "0") int page) {
         return JsonBody.of(
                 HttpStatus.OK.value(),
                 "음악 조회 성공",
