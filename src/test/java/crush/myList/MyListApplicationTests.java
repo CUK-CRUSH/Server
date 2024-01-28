65884b56a8baaa8d7b1f951f00be5e8beac97c64
Feedbaek <nicek789@naver.com>
2024-01-28 23:37:07 +0900

    Feat: image 파일 webp 확장자로 변경 후 업로드로 수정

--- src/test/java/crush/myList/MyListApplicationTests.java
+++ src/test/java/crush/myList/MyListApplicationTests.java
@@ -2,6 +2,7 @@ package crush.myList;
 
 import crush.myList.domain.image.dto.ImageDto;
 import crush.myList.domain.image.service.ImageService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,16 +24,19 @@ class MyListApplicationTests {
     @Autowired
     private ImageService imageService;
     @Test
+//    @Disabled
     @Transactional
     @WithMockUser(username = "test")
     @DisplayName("이미지 파일 GCS에 저장 및 삭제 테스트.")
-    void saveImageToGcsTest() {
+    void saveImageToGcsTest() throws IOException {
         // given
-        MockMultipartFile imageFile = new MockMultipartFile("image", "image.jpg", "image/jpeg", "image".getBytes());
+        URL resource = getClass().getResource("/img/test.png");
+        FileInputStream file = new FileInputStream(resource.getFile());
+        MockMultipartFile imageFile = new MockMultipartFile("image", "image.jpg", "image/jpeg", file);
 
         // when
         ImageDto imageDto = imageService.saveImageToGcs(imageFile);
-        imageService.deleteImageToGcs(imageDto.getId());
+//        imageService.deleteImageToGcs(imageDto.getId());
 
         // then
         assertThat(imageDto.getUrl()).isNotNull();
