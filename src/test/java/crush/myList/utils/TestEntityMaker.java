1aa7810e9540339ebb2a7e30237d17c269372fa0
Feedbaek <nicek789@naver.com>
2024-02-02 15:12:33 +0900

    Fix: 테스트 오류 수정

--- src/test/java/crush/myList/utils/TestEntityMaker.java
+++ src/test/java/crush/myList/utils/TestEntityMaker.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import crush.myList.domain.image.entity.Image;
 import crush.myList.domain.member.entity.Member;
 import crush.myList.domain.member.entity.Role;
+import crush.myList.domain.member.enums.RoleName;
 import crush.myList.domain.music.entity.Music;
 import crush.myList.domain.playlist.entity.Playlist;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,14 +16,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class TestEntityMaker {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    /** 새로운 Role 엔티티 생성 */
+    public Role createRole(RoleName roleName) {
+        return Role.builder()
+                .name(roleName)
+                .build();
+    }
+
     /** 새로운 Member 엔티티 생성 */
-    public Member createUserMember(Role role) {
+    public Member createMember(Role role) {
         return Member.builder()
                 .oauth2id("테스트 oauth2id")
                 .username("테스트 닉네임")
                 .name("테스트 이름")
                 .introduction("테스트 소개")
                 .role(role)
+                .build();
+    }
+
+    public Member createDefaultMember() {
+        return Member.builder()
+                .id(1L)
+                .oauth2id("테스트 oauth2id")
+                .username("테스트 닉네임")
+                .name("테스트 이름")
+                .introduction("테스트 소개")
+                .role(createRole(RoleName.USER))
                 .build();
     }
 
