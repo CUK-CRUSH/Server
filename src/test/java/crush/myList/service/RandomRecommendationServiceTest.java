015842c22f32750ff3df2fd92fd3854413aa951f
DEMl4N <delfirmamentos@gmail.com>
2024-02-13 15:15:59 +0900

    Test: 추천 플레이리스트 Dto 변경에 따른 테스트코드 작성

--- src/test/java/crush/myList/service/RandomRecommendationServiceTest.java
+++ src/test/java/crush/myList/service/RandomRecommendationServiceTest.java
@@ -4,9 +4,9 @@ import crush.myList.config.security.SecurityMember;
 import crush.myList.domain.member.entity.Member;
 import crush.myList.domain.member.repository.MemberRepository;
 import crush.myList.domain.music.Repository.MusicRepository;
-import crush.myList.domain.playlist.dto.PlaylistDto;
 import crush.myList.domain.playlist.entity.Playlist;
 import crush.myList.domain.playlist.repository.PlaylistRepository;
+import crush.myList.domain.recommendation.dto.RecommendationDto;
 import crush.myList.domain.recommendation.service.RandomRecommendationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,10 +19,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("RandomRecommendationService 테스트")
@@ -43,20 +41,20 @@ public class RandomRecommendationServiceTest {
         // given
         given(playlistRepository.findAll()).willReturn(
                 List.of(
-                        Playlist.builder().id(1L).name("테스트1").build(),
-                        Playlist.builder().id(2L).name("테스트2").build(),
-                        Playlist.builder().id(3L).name("테스트3").build(),
-                        Playlist.builder().id(4L).name("테스트4").build(),
-                        Playlist.builder().id(5L).name("테스트5").build(),
-                        Playlist.builder().id(6L).name("테스트6").build(),
-                        Playlist.builder().id(7L).name("테스트7").build(),
-                        Playlist.builder().id(8L).name("테스트8").build()
+                        Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(1L).name("테스트1").build(),
+                        Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(2L).name("테스트2").build(),
+                        Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(3L).name("테스트3").build(),
+                        Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(4L).name("테스트4").build(),
+                        Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(5L).name("테스트5").build(),
+                        Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(6L).name("테스트6").build(),
+                        Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(7L).name("테스트7").build(),
+                        Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(8L).name("테스트8").build()
                 )
         );
         given(musicRepository.countByPlaylist(any(Playlist.class))).willReturn(10L);
 
         // when
-        List<PlaylistDto.Response> recommendation = randomRecommendationService.getRecommendation(null);
+        List<RecommendationDto.Response> recommendation = randomRecommendationService.getRecommendation(null);
 
         // then
         assertThat(recommendation).hasSize(4);
@@ -75,21 +73,21 @@ public class RandomRecommendationServiceTest {
         given(playlistRepository.findAllExceptMember(any())).willReturn(
                 new ArrayList<>(
                         List.of(
-                                Playlist.builder().id(1L).name("테스트1").build(),
-                                Playlist.builder().id(2L).name("테스트2").build(),
-                                Playlist.builder().id(3L).name("테스트3").build(),
-                                Playlist.builder().id(4L).name("테스트4").build(),
-                                Playlist.builder().id(5L).name("테스트5").build(),
-                                Playlist.builder().id(6L).name("테스트6").build(),
-                                Playlist.builder().id(7L).name("테스트7").build(),
-                                Playlist.builder().id(8L).name("테스트8").build()
+                                Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(1L).name("테스트1").build(),
+                                Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(2L).name("테스트2").build(),
+                                Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(3L).name("테스트3").build(),
+                                Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(4L).name("테스트4").build(),
+                                Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(5L).name("테스트5").build(),
+                                Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(6L).name("테스트6").build(),
+                                Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(7L).name("테스트7").build(),
+                                Playlist.builder().member(Member.builder().id(1L).username("Test").build()).id(8L).name("테스트8").build()
                         )
                 )
         );
         given(musicRepository.countByPlaylist(any(Playlist.class))).willReturn(10L);
 
         // when
-        List<PlaylistDto.Response> recommendation = randomRecommendationService.getRecommendation(
+        List<RecommendationDto.Response> recommendation = randomRecommendationService.getRecommendation(
                 SecurityMember.builder().id(1L).username("test").build()
         );
 
