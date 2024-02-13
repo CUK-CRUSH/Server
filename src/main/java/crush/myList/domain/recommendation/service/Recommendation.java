7b87a83bb57662aa4cef1a87d7bbbe3828c9299f
DEMl4N <delfirmamentos@gmail.com>
2024-02-13 15:15:35 +0900

    Feat: 추천 플레이리스트 Response에 유저네임 추가

--- src/main/java/crush/myList/domain/recommendation/service/Recommendation.java
+++ src/main/java/crush/myList/domain/recommendation/service/Recommendation.java
@@ -1,10 +1,10 @@
 package crush.myList.domain.recommendation.service;
 
 import crush.myList.config.security.SecurityMember;
-import crush.myList.domain.playlist.dto.PlaylistDto;
+import crush.myList.domain.recommendation.dto.RecommendationDto;
 
 import java.util.List;
 
 public interface Recommendation {
-    List<PlaylistDto.Response> getRecommendation(SecurityMember member);
+    List<RecommendationDto.Response> getRecommendation(SecurityMember member);
 }
