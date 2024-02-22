41e308bc4f9387f5e3e42c2c7fe2e9e202ba3235
DEMl4N <delfirmamentos@gmail.com>
2024-02-22 14:26:36 +0900

    Feat: 내가 좋아요한 플리 페이지 기능 추가

--- src/main/java/crush/myList/domain/playlist/repository/PlaylistLikeRepository.java
+++ src/main/java/crush/myList/domain/playlist/repository/PlaylistLikeRepository.java
@@ -6,6 +6,7 @@ import crush.myList.domain.playlist.entity.PlaylistLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +15,7 @@ public interface PlaylistLikeRepository extends JpaRepository<PlaylistLike, Long
     Integer countByPlaylistId(Long playlistId);
     List<PlaylistLike> findAllByMemberId(Long memberId);
 
-    List<PlaylistLike> findAllByMember(Member member);
+    List<PlaylistLike> findAllByMember(Member member, Pageable pageable);
 
     Boolean existsByPlaylistIdAndMemberId(Long playlistId, Long memberId);
 
