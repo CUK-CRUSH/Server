2006813d80f56532db3fc5002375c9ec6c7b716e
DEMl4N <delfirmamentos@gmail.com>
2024-02-26 13:36:54 +0900

    Feat: 방명록 / 플레이리스트 좋아요 조회 페이징 (크기 15)

--- src/main/java/crush/myList/domain/playlist/repository/GuestBookRepository.java
+++ src/main/java/crush/myList/domain/playlist/repository/GuestBookRepository.java
@@ -1,7 +1,8 @@
 package crush.myList.domain.playlist.repository;
 
-import crush.myList.domain.playlist.dto.GuestBookDto;
 import crush.myList.domain.playlist.entity.GuestBook;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface GuestBookRepository extends JpaRepository<GuestBook, Long> {
-    List<GuestBook> findAllByPlaylistId(Long playlistId);
+    Page<GuestBook> findAllByPlaylistId(Long playlistId, Pageable pageable);
 }
