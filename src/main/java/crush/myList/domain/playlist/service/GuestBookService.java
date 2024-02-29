ce75f03304258229971e4bb687ac04900762e78b
Feedbaek <nicek789@naver.com>
2024-02-29 20:07:31 +0900

    Refacor: 주석 수정

--- src/main/java/crush/myList/domain/playlist/service/GuestBookService.java
+++ src/main/java/crush/myList/domain/playlist/service/GuestBookService.java
@@ -65,8 +65,10 @@ public class GuestBookService {
         Member guestBookMember = guestBook.getMember();
         Member playlistOwner = playlist.getMember();
 
-        if (!playlist.getId().equals(playlistId) ||  // playlistId가 일치하지 않는 경우
-                (!guestBookMember.getId().equals(member.getId())) && !guestBookMember.getId().equals(playlistOwner.getId()) ) {  // 방명록 작성자가 아니고 플레이리스트 주인이 아닌 경우
+        // playlistId가 일치하지 않는 경우 또는 방명록 작성자가 아니고 플레이리스트 주인이 아닌 경우
+        if (!playlist.getId().equals(playlistId) ||
+                // 방명록 작성자가 아니고 플레이리스트 주인이 아닌 경우
+                (!guestBookMember.getId().equals(member.getId())) && !guestBookMember.getId().equals(playlistOwner.getId()) ) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "방명록을 삭제할 수 없습니다.");
         }
 
