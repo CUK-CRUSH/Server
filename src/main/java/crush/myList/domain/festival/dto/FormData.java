e802e58fd138a875ba7f7f391f79aa5629434f2d
Feedbaek <nicek789@naver.com>
2024-03-08 13:13:25 +0900

    Docs: dto 설명 추가

--- src/main/java/crush/myList/domain/festival/dto/FormData.java
+++ src/main/java/crush/myList/domain/festival/dto/FormData.java
@@ -1,5 +1,6 @@
 package crush.myList.domain.festival.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
@@ -10,15 +11,21 @@ import lombok.*;
 @AllArgsConstructor
 public class FormData {
     @NotBlank
+    @Schema(description = "나이", example = "20")
     private String age;
     @NotBlank
+    @Schema(description = "성별", example = "남자")
     private String sex;
     @NotBlank
+    @Schema(description = "전화번호", example = "010-1234-5678")
     private String phone;
     @NotBlank
+    @Schema(description = "이름", example = "홍길동")
     private String name;
     @NotBlank
+    @Schema(description = "url", example = "https://mylist.im/user/username")
     private String link;
     @NotBlank
+    @Schema(description = "장르", example = "힙합")
     private String genre;
 }
--- src/main/java/crush/myList/domain/search/dto/MemberDto.java
+++ src/main/java/crush/myList/domain/search/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package crush.myList.domain.search.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,8 +9,12 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDto {
+    @Schema(name = "id", description = "사용자 ID", example = "1")
     private Long id;
+    @Schema(name = "username", description = "사용자 닉네임", example = "crush")
     private String username;
+    @Schema(name = "introduction", description = "사용자 소개", example = "안녕하세요!")
     private String introduction;
+    @Schema(name = "profileImageUrl", description = "사용자 프로필 이미지 URL")
     private String profileImageUrl;
 }
